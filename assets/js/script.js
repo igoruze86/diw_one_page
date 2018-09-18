c0a6cab417bd1293ee467075b9ecc2dc37e520b2
Igor Uzelac <igor.uzelac@diwanee.com>
2018-09-18 13:58:24 +0200

    Commit#30-css update

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,62 +25,75 @@ function validateForm() {
       if (ime == "") {
         msg.slideUp(300, function(){
           msg.removeClass("success_msg");
+          msg.removeClass("error_msg");
           msg.addClass("error_msg");
           feedback += error;
           document.getElementById('feedback').innerHTML = feedback;
           document.getElementById('ime').style.borderColor = 'red';
           msg.slideDown(300);
         })
+        return false;
       }
       else if (email == '') {
         msg.slideUp(300, function(){
           msg.removeClass("success_msg");
+          msg.removeClass("error_msg");
+          msg.addClass("feedback_display_none");
           msg.addClass("error_msg");
           feedback += error;
           document.getElementById('feedback').innerHTML = feedback;
           document.getElementById('email').style.borderColor = 'red';
           msg.slideDown(300);
         })
+        return false;
       }
       else if (at < 1 || dot < 1) {
         msg.slideUp(300, function(){
           msg.removeClass("success_msg");
+          msg.removeClass("error_msg");
           msg.addClass("error_msg");
           feedback += error;
           document.getElementById('feedback').innerHTML = feedback;
           document.getElementById('email').style.borderColor = 'red';
           msg.slideDown(300);
         })
+        return false;
       }
       else if (at > dot) {
         msg.slideUp(300, function(){
           msg.removeClass("success_msg");
+          msg.removeClass("error_msg");
           msg.addClass("error_msg");
           feedback += error;
           document.getElementById('feedback').innerHTML = feedback;
           document.getElementById('email').style.borderColor = 'red';
           msg.slideDown(300);
         })
+        return false;
       }
       else if (checkbox == false) {
         msg.slideUp(300, function(){
           msg.removeClass("success_msg");
+          msg.removeClass("error_msg");
           msg.addClass("error_msg");
           feedback += error;
           document.getElementById('feedback').innerHTML = feedback;
           document.getElementById('boxlabel').style.color = 'red';
           msg.slideDown(300);
         })
+        return false;
       }
       else if (poruka == '') {
         msg.slideUp(300, function(){
           msg.removeClass("success_msg");
+          msg.removeClass("error_msg");
           msg.addClass("error_msg");
           feedback += error;
           document.getElementById('feedback').innerHTML = feedback;
           document.getElementById('msglabel').style.color = 'red';
           msg.slideDown(300);
         })
+        return false;
       }
       else if (feedback == '') {
         msg.slideUp(300, function(){
@@ -94,6 +107,7 @@ function validateForm() {
           document.getElementById('msglabel').style.color = '#919191';
           msg.slideToggle(300);
         })
+        return true;
       }
     })
   }
